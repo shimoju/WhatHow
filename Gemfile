a1f20676bca37a94b07b48e08bb02e2155473aa5
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2014-09-27 22:40:06 +0900

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'sinatra-contrib'
+
+gem 'foreman'
+gem 'puma'
+gem 'omniauth-twitter'
+gem 'twitter'
---
+++ app.rb
@@ -0,0 +1,55 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+require 'omniauth-twitter'
+require 'twitter'
+
+class App < Sinatra::Base
+  configure do
+    enable :sessions
+
+    set twitter_api_key: ENV['TWITTER_API_KEY'], twitter_api_secret: ENV['TWITTER_API_SECRET']
+    use OmniAuth::Builder do
+      provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
+    end
+  end
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  helpers do
+    def create_twitter_client
+      Twitter::REST::Client.new do |config|
+        config.consumer_key = settings.twitter_api_key
+        config.consumer_secret = settings.twitter_api_secret
+        config.access_token = session[:user][:token]
+        config.access_token_secret = session[:user][:secret]
+      end
+    end
+  end
+
+  get '/' do
+    unless session[:user]
+      erb :index
+    else
+      erb :home
+    end
+  end
+
+  get '/auth/:provider/callback' do
+    auth = request.env['omniauth.auth']
+    session[:user] = {
+      name: auth.info.name,
+      username: auth.info.nickname,
+      image: auth.info.image,
+      token: auth.credentials.token,
+      secret: auth.credentials.secret
+    }
+    redirect to('/')
+  end
+
+  get '/logout' do
+    session[:user] = nil
+    redirect to('/')
+  end
+end
