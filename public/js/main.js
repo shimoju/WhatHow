690f79c049890c4c5e4734f0ae51d8a65a4c2ca4
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2014-09-29 23:41:14 +0900

    ボタンを押したらテキスト追加、ツイートできるようにする

---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+$(function() {
+  $('.emotion').click(function() {
+    var how = document.getElementById('how');
+    how.value += $(this).text();
+  });
+
+  $('#btn-tweet').click(function() {
+    // 別ウインドウで開くようにする
+    this.target = '_blank'
+
+    // <textarea>要素の内容を取得し投稿内容を作成
+    var tweet = {
+      text: document.getElementById('what').value + document.getElementById('how').value,
+      hashtags: 'whathow'
+    };
+    // 投稿内容のパラメータをquery stringに変換し、URLを作成
+    // ex) https://twitter.com/intent/tweet?text=TweetText&hashtags=whathow
+    var url = 'https://twitter.com/intent/tweet?' + $.param(tweet);
+    this.href = url;
+  });
+
+});
